37e2776d3c5452e28dfdc5a0fcc3694be3f96dc4
galynary <107271972+galynary@users.noreply.github.com>
2022-12-03 16:34:47 +0200

    change video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,42 +1,22 @@
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = "videoplayer-current-time";
 
-
-let currentTime;
-try {
-  currentTime = Number(localStorage.getItem('videoplayer-current-time'));
-} catch (error) {
-  currentTime = 0;
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+   
+function onPlay ({ seconds }) {
+    localStorage.setItem(CURRENT_TIME, seconds)
 }
-
-let iframe = document.querySelector('iframe');
-let player = new Player(iframe);
-
-player
-  .setCurrentTime(Number(currentTime))
-  .then(function (seconds) {
-    console.log(`ok - ${seconds}`);
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        currentTime = 0;
-        break;
-
-      default:
-        console.log(error);
-        break;
+setCurrentTime()
+function setCurrentTime(){
+    if(!localStorage.getItem(CURRENT_TIME)){
+        return
     }
-  });
-
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, 1000),
-);
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME))
+}
 
 
 
